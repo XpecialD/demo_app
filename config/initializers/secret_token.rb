d367a02ca98b2c0d0f1738c728c589833d35e29d
Xdeveloper <dev@xpeciald.com>
2012-12-05 09:36:09 -0800

    initial demo commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '2cd1a1e34c46f53a07a8610a892b12054b44679bd73a9d7bd62cd0b9d603042c91708bf740f00c735ba65c57f09d8b859ecbf1ec3af784d58f1697423124e8e2'
